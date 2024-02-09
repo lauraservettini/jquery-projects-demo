b95ae91c3e19db42ff9eda6171a5fe8bdfd70574
lauraservettini <lauraservettini87@gmail.com>
2024-02-09 11:54:58 +0100

    fix a bug on the menu

--- script/mobile-navbar.js
+++ script/mobile-navbar.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
         $('#mobile').slideToggle(2000);
     })
 })
+
+$( window ).on( "resize", function() {
+    if ($(window).width() > 640){
+        $('#mobile').css('display', 'none');
+    }
+  } );
